351638dd3458f620cbe17582ee988955288f477d
SuperHotDog <115141367+SuperHotDogCat@users.noreply.github.com>
2024-11-18 18:46:28 +0900

    Update custom_allocator_experiment.sh

--- custom_allocator_experiment.sh
+++ custom_allocator_experiment.sh
@@ -1,15 +1,15 @@
 make clean
 make
 echo "Normal cudaMalloc Test"
-rye run python simple_training_process.py
+python simple_training_process.py
 echo ""
 make clean
 make CXXFLAGS="-fPIC -O3 -DMANAGED"
 echo "cudaManagedMalloc Test"
-rye run python simple_training_process.py
+python simple_training_process.py
 echo ""
 make clean
 make CXXFLAGS="-fPIC -O3 -DCPU"
 echo "cudaMallocHost Test"
-rye run python simple_training_process.py
+python simple_training_process.py
 echo ""
